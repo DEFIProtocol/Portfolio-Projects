f6c126da814635cd625a188ba756f336538638ca
DEFIProtocol <Beauallgood@gmail.com>
2022-09-24 19:38:14 -0500

    adjust project page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,6 +13,8 @@ const style = {
         height: "250px",
         width: "250px",
         margin: "15px",
+        display: "flex",
+        alignItems: "center",
     },
     projectDescription: {
     },
@@ -20,7 +22,9 @@ const style = {
         color: "red",
         margin: "0px auto",
         padding: "5px",
-        fontWeight: "bold"
+        fontWeight: "bold",
+        display: "flex",
+        alignItems: "center",
     },
     linksAndDesc: {
         float: "right",
@@ -30,7 +34,10 @@ const style = {
         backgroundColor: "#202020",
         color: "#909090",
         margin: "15px", 
-        padding: "20px"
+        padding: "20px",
+        display: "flex",
+        alignItems: "center",
+        
     }
 }
 
@@ -50,37 +57,38 @@ function Projects() {
                 cloudName="gridlock"
                 style={style.image}
                 />
+                        <a href="https://github.com/DEFIProtocol/StarterCryptoProject2" style={{position: "absolute", color: "white", fontSize: "150%", marginLeft: "15%" }}>
+                            <GithubOutlined style={{position: "absolute", color: "white", fontSize: "150%"}}>
+                            </GithubOutlined>
+                        </a>
+                        <a href="./Projects/mother-teresa/client/src/App.js" style={{position: "absolute", color: "white", fontSize: "150%" , marginLeft: "30%"}}>
+                        <ProjectOutlined style={{position: "absolute", color: "white", fontSize: "150%"}}/>
+                        </a>
                         <span style={{float: "right", width: "60%", marginTop: "10%"}}>
                             An application capable of viewing top 10 crypto news, and all prices for 100 different cryptocurrencies.<br /> <br />Server: MongoDB <br />API: Rapid API <br /> Packages: React, Ant-Design, ChartJs
                         </span>
-                        <a href="https://github.com/DEFIProtocol/StarterCryptoProject2" style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right", marginLeft: "33%", marginTop: "5%" }}>
-                            <GithubOutlined style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right" }}>
-                            </GithubOutlined>
-                        </a>
-                        <a href="./Projects/mother-teresa/client/src/App.js" style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right", marginLeft: "50%", marginTop: "5%" }}>
-                        <ProjectOutlined style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right"}}/>
-                        </a>
                 </Card>
             </div>
             <div>
                 <Card style={style.card}>
-                    <span style={style.warning}>** In order to view entire project must have a compatible browser wallet. **</span>
-                    <span style={style.warning}>** You can see full video walk-through <a href="https://youtu.be/MlUy0TSYOQg">Here</a> **</span>
+                    <span style={style.warning}>** In order to view entire project must have a compatible browser wallet. ** <br />
+                    ** You can see full video walk-through
+                    <a href="https://youtu.be/MlUy0TSYOQg" style={{color: "lime", textDecoration: "none", display: "inline-block"}}>Here</a> **</span>
                     <Image
                 publicId={"ptphqpxirxjdwdx0apgc"}
                 cloudName="gridlock"
                 style={style.image}
                 />
+                        <a src="https://github.com/DEFIProtocol/lock" style={{position: "absolute", color: "white", fontSize: "150%" , marginLeft: "15%"}}>
+                            <GithubOutlined style={{position: "relative", color: "white", fontSize: "150%"}}>
+                            </GithubOutlined>
+                        </a>
+                        <a src="" style={{position: "absolute", color: "white", fontSize: "150%" , marginLeft: "30%"}}>
+                            <ProjectOutlined style={{position: "relative", color: "white", fontSize: "150%"}}/>
+                        </a>
                         <span style={{float: "right", width: "60%", marginTop: "10%"}}>
                             An application with the capabilities of displaying all information for different fungible tokens. Data is pulled directly from the blockchain, and various API's.<br /> <br />Server: Moralis <br />API: Moralis, Infura(RPCNODE) <br /> Packages: React, Moralis, Ant-Design, ChartJs
                         </span>
-                        <a src="https://github.com/DEFIProtocol/lock" style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right", marginLeft: "33%", marginTop: "5%" }}>
-                            <GithubOutlined style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right" }}>
-                            </GithubOutlined>
-                        </a>
-                        <a src="" style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right", marginLeft: "50%", marginTop: "5%" }}>
-                            <ProjectOutlined style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right"}}/>
-                        </a>
                 </Card>
             <div>
             <h3 style={style.heading}>Data Analysis</h3>
@@ -90,16 +98,16 @@ function Projects() {
                 cloudName="gridlock"
                 style={style.image}
                 />
+                    <a src="https://github.com/DEFIProtocol/House-Property-Sales" style={{position: "absolute", color: "white", fontSize: "150%" , marginLeft: "15%"}}>
+                        <GithubOutlined style={{position: "relative", color: "white", fontSize: "150%"}}>
+                        </GithubOutlined>
+                    </a>
+                    <a src="./Projects/gridlock/src/App.jsx" style={{position: "absolute", color: "white", fontSize: "150%" , marginLeft: "30%" }}>
+                        <ProjectOutlined style={{position: "relative", color: "white", fontSize: "150%" }}/>
+                    </a>
                     <span style={{float: "right", width: "60%", marginTop: "10%"}}>
                         Using psycopg2 retrieved data from SQL server, and ordered the data by dates. The x-axis is the number of homes sold monthly, and the y-axis corresponds with the month starting in Feb 2007 to 2018.<br /> <br />Server: PostgreSQL <br />Data: Kaggle(House Property Sales Time Series) <br /> Libraries: MatPlotLib, Psycopg2
                     </span>
-                    <a src="https://github.com/DEFIProtocol/House-Property-Sales" style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right", marginLeft: "33%", marginTop: "5%" }}>
-                        <GithubOutlined style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right" }}>
-                        </GithubOutlined>
-                    </a>
-                    <a src="./Projects/gridlock/src/App.jsx" style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right", marginLeft: "50%", marginTop: "5%" }}>
-                        <ProjectOutlined style={{position: "relative", color: "white", fontSize: "150%", width: "25%", float: "right"}}/>
-                    </a>
             </Card>
         </div>
         </div>
